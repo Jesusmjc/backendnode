3ef86f832ef977cdbd468fa033602e69471be2b1
jesusmjc <jesusmanuelmujica12@gmail.com>
2024-05-15 23:00:57 -0600

    auth

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-
 var corsOptions = {
     origin: ["http://localhost:3001", "http://localhost:8080"],
     methods: "GET, PUT, POST, DELETE",
@@ -23,7 +21,14 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use("/api/categorias", require('./routes/categorias.routes'))
 app.use("/api/peliculas", require('./routes/peliculas.routes'))
+app.use("/api/usuarios", require('./routes/usuarios.routes'))
+app.use("/api/roles", require('./routes/roles.routes'))
+app.use("/api/auth", require('./routes/auth.routes'))
 app.get('*', (req, res) => { res.status(404).send() });
+
+const errorlogger = require('./middlewares/errorlogger.middleware')
+const errorhandler = require('./middlewares/errorhandler.middlewares')
+app.use(errorlogger, errorhandler)
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log("Aplicación de ejemplo escuchando en el puerto " + process.env.SERVER_PORT)
---
+++ routes/auth.routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const auth = require('../controllers/auth.controller');
+const Authorize = require('../middlewares/auth.middleware');
+
+// POST: api/auth
+router.post('/', auth.login);
+
+// GET: api/auth7tiempo
+router.get('/tiempo', auth.tiempo);
+
+module.exports = router
+
